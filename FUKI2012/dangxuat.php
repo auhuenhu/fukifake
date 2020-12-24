f7ee1f3f89099cc84675b4f4b407e5106b71e048
auhuenhu <72957275+auhuenhu@users.noreply.github.com>
2020-12-24 16:55:55 +0700

    Update dangxuat.php

--- FUKI2012/dangxuat.php
+++ FUKI2012/dangxuat.php
@@ -5,12 +5,18 @@ if(isset($_POST['btnDangXuat']))
 
 	if (isset($_SESSION['user']))
 	{
-   unset($_SESSION['user']);
-}
+   unset($_SESSION['user']);}
+   if (isset($_SESSION['ad']))
+	{
+   unset($_SESSION['ad']);}
+
 }
 header('location:index.php');
 
 ?>
+
+
+
 
 
 
